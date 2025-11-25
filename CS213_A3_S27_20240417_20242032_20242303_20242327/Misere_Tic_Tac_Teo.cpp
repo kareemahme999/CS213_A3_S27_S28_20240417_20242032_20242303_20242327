4396a9234aceed9f78b5ad84c1128924680574e4
Al Khateeb <eng.al.khateeb404@gmail.com>
2025-11-25 16:27:57 +0200

    Add CMake configuration and new Misere Tic Tac Toe files

---
+++ CS213_A3_S27_20240417_20242032_20242303_20242327/Misere_Tic_Tac_Teo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mohamed Al Khateeb on 25-Nov-25.
+//
+
+#include "Misere_Tic_Tac_Teo.h"
---
+++ CS213_A3_S27_20240417_20242032_20242303_20242327/Misere_Tic_Tac_Teo.h
@@ -0,0 +1,16 @@
+//
+// Created by Mohamed Al Khateeb on 25-Nov-25.
+//
+
+#ifndef MISERE_TIC_TAC_TEO_H
+#define MISERE_TIC_TAC_TEO_H
+
+
+
+class Misere_Tic_Tac_Teo {
+
+};
+
+
+
+#endif //MISERE_TIC_TAC_TEO_H
